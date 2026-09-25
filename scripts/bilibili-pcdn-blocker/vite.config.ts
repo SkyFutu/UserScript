f5c2726df1fbf6c1d9fcd2e6fc8c688299fbc9ab
SkyFutu <119418823+SkyFutu@users.noreply.github.com>
2026-09-26 04:54:34 +0800

    feat: Bilibili PCDN Blocker v1.0.0

---
+++ scripts/bilibili-pcdn-blocker/vite.config.ts
@@ -0,0 +1,23 @@
+import { createMonkeyConfig } from '../../vite.base.config';
+
+// 现在只需传一个对象，name 也写在这里面
+export default createMonkeyConfig({
+  name: 'Bilibili PCDN Blocker', 
+  version: '1.0.0',
+  description: '屏蔽B站PCDN并直连官方CDN加载',
+  icon: 'https://static.hdslb.com/images/favicon.ico',
+  match: [
+    'https://www.bilibili.com/video/*',
+    'https://www.bilibili.com/list/*',
+    'https://www.bilibili.com/bangumi/play/*',
+    'https://live.bilibili.com/*',
+  ],
+  grant: [
+    'unsafeWindow',
+    'GM_registerMenuCommand',
+    'GM_getValue',
+    'GM_setValue'
+  ],
+  downloadURL: 'https://fastly.jsdelivr.net.jsdelivr.net/gh/SkyFutu/UserScript@build/bilibili-pcdn-blocker.user.js',
+  updateURL: 'https://fastly.jsdelivr.net.jsdelivr.net/gh/SkyFutu/UserScript@build/bilibili-pcdn-blocker.user.js',
+});
